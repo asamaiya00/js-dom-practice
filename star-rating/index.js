af1ceca152b931af038bb7edc2b92bc3ae220cd6
Animesh Samaiya <asamaiya00@gmail.com>
2022-04-26 23:39:11 +0530

    hover event

--- star-rating/index.js
+++ star-rating/index.js
@@ -8,7 +8,20 @@ function getStar(value) {
  * @param callback Returns selected star count to callback
  */
 function Star(el, count, callback) {
-  // write logic to create star rating utility.
+  // Write a logic to create star rating problem
+  let blankStarHtml = `<i class="fa fa-star-o"></i>`;
+
+  document.querySelector(el).innerHTML = blankStarHtml.repeat(count);
+  let starEl = document.getElementById('star');
+  starEl.addEventListener('mouseover', (e) => {
+    if (e.target.tagName === 'I') {
+      console.log('hover');
+      const num = document.querySelectorAll('i.fa ~ i.fa-star-o').length;
+      callback(num);
+      e.target.classList.toggle('fa-star-o');
+      e.target.classList.toggle('fa-star');
+    }
+  });
 }
 
 new Star('#star', 5, getStar);
